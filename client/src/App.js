eb7037db6d2d33a4ca83838a6c6abbab757d3b68
Alberto Pérez de Rada Fiol <albertopdrf.wca@gmail.com>
2020-03-28 14:45:51 +0100

    Install Material-UI.
    https://material-ui.com/.

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Container } from '@material-ui/core';
+import { Typography } from '@material-ui/core';
 
 class App extends React.Component {
   constructor(props) {
@@ -20,23 +20,17 @@ class App extends React.Component {
   
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-        <p className="App-intro">{this.state.apiResponse}</p>
-      </div>
+      <Container maxWidth="sm">
+        <Typography variant="h1">
+          Tisn
+        </Typography>
+        <Typography variant="h2">
+          The introverts' social network
+        </Typography>
+        <Typography variant="body1">
+          {this.state.apiResponse}
+        </Typography>
+      </Container>
     );
   }
 }
