83e569a604a30fdb7a24577d3f86e6bbe2906cfa
san-oop <sanooppoly777@gmail.com>
2020-10-04 11:31:14 +0530

    Dynamic colors for svg based on the active theme

--- client/src/Components/SvgImage/SvgImage.js
+++ client/src/Components/SvgImage/SvgImage.js
@@ -2,28 +2,16 @@ import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { useTheme } from '@material-ui/core/styles';
 
-import { ReactComponent as EventsImageLight } from '../../assets/images/events-light.svg';
-import { ReactComponent as InterestsImageLight } from '../../assets/images/interests-light.svg';
-import { ReactComponent as OpenSourceImageLight } from '../../assets/images/openSource-light.svg';
-import { ReactComponent as UsersImageLight } from '../../assets/images/users-light.svg';
-import { ReactComponent as EventsImageDark } from '../../assets/images/events-dark.svg';
-import { ReactComponent as InterestsImageDark } from '../../assets/images/interests-dark.svg';
-import { ReactComponent as OpenSourceImageDark } from '../../assets/images/openSource-dark.svg';
-import { ReactComponent as UsersImageDark } from '../../assets/images/users-dark.svg';
+import { ReactComponent as EventsImage } from '../../assets/images/events.svg';
+import { ReactComponent as InterestsImage } from '../../assets/images/interests.svg';
+import { ReactComponent as OpenSourceImage } from '../../assets/images/openSource.svg';
+import { ReactComponent as UsersImage } from '../../assets/images/users.svg';
 
 const images = {
-  light: {
-    events: EventsImageLight,
-    interests: InterestsImageLight,
-    openSource: OpenSourceImageLight,
-    users: UsersImageLight,
-  },
-  dark: {
-    events: EventsImageDark,
-    interests: InterestsImageDark,
-    openSource: OpenSourceImageDark,
-    users: UsersImageDark,
-  },
+  users: UsersImage,
+  events: EventsImage,
+  interests: InterestsImage,
+  openSource: OpenSourceImage,
 };
 
 const SvgImage = (props) => {
@@ -32,12 +20,13 @@ const SvgImage = (props) => {
   const { t } = useTranslation();
   const theme = useTheme();
 
-  const Image = images[theme.palette.type][name];
+  const Image = images[name];
 
   return (
     <Image
       alt={t(`svgImage.${name}`)}
       title={t(`svgImage.${name}`)}
+      fill={theme.palette.primary.main}
       {...rest}
     />
   );
