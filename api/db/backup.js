2caddde19232395bb6efb1c28ac15f1c949ebfea
Jack Elendil B. Lagare <jackeblagare@gmail.com>
2020-10-09 18:39:02 +0800

    Enclose code in function for better readability.

--- api/db/backup.js
+++ api/db/backup.js
@@ -6,35 +6,39 @@ const getDbUrl = () => {
   return process.env.DB_URL;
 };
 
-// Prepare database archive filename
-currentDate = new Date();
+const createBackup = () => {
+  // Prepare database archive filename
+  currentDate = new Date();
 
-let databaseDumpFilename =
-  'tisn_db_dump_' +
-  currentDate.getFullYear() +
-  '_' +
-  (currentDate.getMonth() + 1) +
-  '_' +
-  currentDate.getDate() +
-  '.gz';
+  let databaseDumpFilename =
+    'tisn_db_dump_' +
+    currentDate.getFullYear() +
+    '_' +
+    (currentDate.getMonth() + 1) +
+    '_' +
+    currentDate.getDate() +
+    '.gz';
 
-// Prepare mongodump command
-let dumpCmd =
-  "mongodump --uri '" +
-  getDbUrl() +
-  "' --archive=dump/" +
-  databaseDumpFilename +
-  ' --gzip';
+  // Prepare mongodump command
+  let dumpCmd =
+    "mongodump --uri '" +
+    getDbUrl() +
+    "' --archive=dump/" +
+    databaseDumpFilename +
+    ' --gzip';
 
-// Execute mongodump command
-exec(dumpCmd, (error, stdout, stderr) => {
-  if (error) {
-    console.log(error);
-    return 0;
-  } else {
-    console.log(
-      'Successfully created database dump at dump/' + databaseDumpFilename
-    );
-    return 1;
-  }
-});
+  // Execute mongodump command
+  exec(dumpCmd, (error, stdout, stderr) => {
+    if (error) {
+      console.log(error);
+      return 0;
+    } else {
+      console.log(
+        'Successfully created database dump at dump/' + databaseDumpFilename
+      );
+      return 1;
+    }
+  });
+};
+
+module.exports = { createBackup };
