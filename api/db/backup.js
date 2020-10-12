14535f124a2ab3ddfd560e18bceccae9bbde1e92
Jack Elendil B. Lagare <jackeblagare@gmail.com>
2020-10-12 15:11:06 +0800

    Add --oplog parameter to mongodump.

--- api/db/backup.js
+++ api/db/backup.js
@@ -7,11 +7,10 @@ const createBackup = () => {
 
   const now = new Date();
 
-  const dumpFilename = `${now.getFullYear()}${
-    now.getMonth() + 1
-  }${now.getDate()}.gz`;
+  const dumpFilename = `${now.getFullYear()}${now.getMonth() + 1
+    }${now.getDate()}.gz`;
 
-  const dumpCmd = `mongodump --uri='${getDbUrl()}' --gzip --archive='${dumpsPath}/${dumpFilename}'`;
+  const dumpCmd = `mongodump --uri='${getDbUrl()}' --gzip --archive='${dumpsPath}/${dumpFilename}' --oplog`;
 
   exec(dumpCmd, (error, stdout, stderr) => {
     if (!error) require('../utils/emails').dbBackup(dumpFilename);
