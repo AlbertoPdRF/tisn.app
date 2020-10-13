fb3dca2bb3dcdae320a3207070b43d1852974e38
Jack Elendil B. Lagare <jackeblagare@gmail.com>
2020-10-13 08:48:50 +0800

    Reformat MongoDB URI to allow full database dumps.

--- api/db/backup.js
+++ api/db/backup.js
@@ -1,16 +1,22 @@
 const exec = require('child_process').exec;
 const path = require('path');
+const parser = require('mongodb-uri');
 const { getDbUrl } = require('./connection');
 
 const createBackup = () => {
+  // Remove database name from URI to allow full database dumps
+  const uri = parser.parse(getDbUrl());
+  const mongoDbUrl = `${uri['scheme']}://${uri['username']}:${uri['password']}@${uri['hosts'][0]['host']}/`;
+
   const dumpsPath = path.join(__dirname, 'dumps');
 
   const now = new Date();
 
-  const dumpFilename = `${now.getFullYear()}${now.getMonth() + 1
-    }${now.getDate()}.gz`;
+  const dumpFilename = `${now.getFullYear()}${
+    now.getMonth() + 1
+  }${now.getDate()}.gz`;
 
-  const dumpCmd = `mongodump --uri='${getDbUrl()}' --gzip --archive='${dumpsPath}/${dumpFilename}' --oplog`;
+  const dumpCmd = `mongodump --uri='${mongoDbUrl}' --gzip --archive='${dumpsPath}/${dumpFilename}' --oplog`;
 
   exec(dumpCmd, (error, stdout, stderr) => {
     if (!error) require('../utils/emails').dbBackup(dumpFilename);
