bf30fe15a135e9f14fee6ace701136701d16b11e
Jack Elendil B. Lagare <jackeblagare@gmail.com>
2020-10-14 14:43:21 +0800

    Add --oplog parameter to mongodump (#343)
    * Add --oplog parameter to mongodump.
    
    * Add mongodb-uri as a project dependency.
    
    * Reformat MongoDB URI to allow full database dumps.
    
    * Implement a cleaner approach to formatting the URI.
    
    * Update package-log.json.
    
    * Remove unused lien of code.
    
    * Add mongodb-uri as a dependency.
    
    * Reset packages.
    
    * Revert to old dependencies.
    
    * Install mongodb-uri.

--- api/db/backup.js
+++ api/db/backup.js
@@ -1,8 +1,15 @@
 const exec = require('child_process').exec;
 const path = require('path');
+const mongodbUri = require('mongodb-uri');
 const { getDbUrl } = require('./connection');
 
 const createBackup = () => {
+  // Remove database (and options) from URI to allow full database dumps
+  const parsedUri = mongodbUri.parse(getDbUrl());
+  delete parsedUri.database;
+  delete parsedUri.options;
+  const uri = mongodbUri.format(parsedUri);
+
   const dumpsPath = path.join(__dirname, 'dumps');
 
   const now = new Date();
@@ -11,7 +18,7 @@ const createBackup = () => {
     now.getMonth() + 1
   }${now.getDate()}.gz`;
 
-  const dumpCmd = `mongodump --uri='${getDbUrl()}' --gzip --archive='${dumpsPath}/${dumpFilename}'`;
+  const dumpCmd = `mongodump --uri='${uri}' --gzip --archive='${dumpsPath}/${dumpFilename}' --oplog`;
 
   exec(dumpCmd, (error, stdout, stderr) => {
     if (!error) require('../utils/emails').dbBackup(dumpFilename);
